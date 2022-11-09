4914d868e970b8aa417ab30c69c828e37d411576
Gabriel Cards <gabriel.cardoso@trinto.com.br>
2022-11-09 12:31:45 -0300

    feat(header): made header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import {Container} from "./style"
+
+export default function Header() {
+    return (
+        <Container>
+            <img src="./triangle.png"/>
+            <h1>Controle Financeiro</h1>
+            <img src="./menu-hamb.png"/>
+        </Container>
+    )
+}
